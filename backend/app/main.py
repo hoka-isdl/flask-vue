9cb5ef55285cc210042c4da74ab5c3ce40be3216
hoka <oka.hayato@mikilab.doshisha.ac.jp>
2022-12-10 14:11:58 +0900

    first Commit

---
+++ backend/app/main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, jsonify, request, make_response
+from flask_restful import Api, Resource
+from flask_cors import CORS
+from random import *
+# from isbn import RAKUTENAPI
+# from sql import sql_arg
+# RA = RAKUTENAPI()
+# SQLARG = sql_arg()
+
+# FlaskとVueの連携
+app = Flask(__name__, static_folder='../../frontend/dist/static', template_folder='../../frontend/dist')
+# 日本語
+app.config['JSON_AS_ASCII'] = False
+# CORS=Ajaxで安全に通信するための規約
+api = Api(app)
+CORS(app)
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
