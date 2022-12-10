f145dbf07d6f46a333ddfd049f71e794bda065ab
hoka <oka.hayato@mikilab.doshisha.ac.jp>
2022-12-10 15:48:09 +0900

    first commit

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <div>
+      <form v-on:click.prevent>
+        <label>Email: </label>
+        <input type="text" required v-model="email" />
+
+        <label>Password: :</label>
+        <input type="password" v-model="password" >
+
+        <div class="submit">
+          <button v-on:click="userSignIn">ログイン</button>
+        </div>
+        
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+form {
+  max-width: 420px;
+  margin: 0 auto;
+  background-color: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+label {
+  color: #aaa;
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+input,
+select {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+input[type='checkbox'] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
+}
+h4 {
+  margin-bottom: 0;
+}
+.pill {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background-color: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
+}
+button {
+  background-color: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+}
+.submit {
+  text-align: center;
+}
+.error {
+  color: #ff0062;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
+}
+</style>
+
